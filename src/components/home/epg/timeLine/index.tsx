08fbce3098d220b5744f7cf208bd628587ba1912
Samuel Hoyas <samuelix97@gmail.com>
2024-01-29 19:38:38 +0100

    Time line css

--- src/components/home/epg/timeLine/index.tsx
+++ src/components/home/epg/timeLine/index.tsx
@@ -4,77 +4,125 @@ import "./_style.scss";
 const EPGTimeLineBar = () => {
   return (
     <div className="epg-time-line-container">
-      <div>
-        <span className="epg-time-line-item">00:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">00:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">01:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">01:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">02:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">02:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">03:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">03:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">04:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">04:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">05:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">05:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">06:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">06:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">07:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">07:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">08:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">08:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">09:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">09:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">10:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">10:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">11:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">11:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">12:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">12:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">13:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">13:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">14:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">14:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">15:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">15:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">16:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">16:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">17:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">17:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">18:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">18:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">19:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">19:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">20:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">20:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">21:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">21:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">22:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">22:00</span>
+        </div>
       </div>
-      <div>
-        <span className="epg-time-line-item">23:00</span>
+      <div className="epg-time-line-item-container">
+        <div className="epg-time-line-item-min-content">
+          <span className="epg-time-line-item">23:00</span>
+        </div>
       </div>
     </div>
   );
