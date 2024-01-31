e459c6252b66dde4f06d7d8f69c1dcd6aba4f9fb
Samuel Hoyas <samuelix97@gmail.com>
2024-01-31 20:24:51 +0100

    Info page CSS

--- src/components/info/index.tsx
+++ src/components/info/index.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ExtendedImage from "src/elements/image";
 import Expandable from "src/elements/expandable";
 import { getProgramInfo } from "./getProgramInfo";
 import { getParsedProgramInfo } from "src/utils/programInfoParser";
 import { ProgramInfoInterface } from "./programInfoInterface";
+import literals from "@assets/literals/eng.json";
 import "./_style.scss";
 
 const InfoPage = ({ id }: { id: string }) => {
   const [programData, setProgramData] = useState<ProgramInfoInterface>();
-  const dateRef = useRef(new Date());
 
   useEffect(() => {
     getProgramInfo(id).then((programData) =>
@@ -16,46 +16,68 @@ const InfoPage = ({ id }: { id: string }) => {
     );
   }, [id]);
 
+  useEffect(() => {
+    document.body.style.backgroundColor = "rgb(0 ,0 ,0)";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   if (!programData) {
     return <></>;
   }
-  console.log(programData);
+
   return (
     <div className="info-page-container">
       <ExtendedImage image={{ image: programData.images.icon }} />
       <div className="info-page-data-container">
         <div className="info-page-header-data">
-          <div className="info-page-header-section">
+          <div className="info-page-header-section info-page-header-channel-logo">
             <ExtendedImage image={{ image: programData.channelImages.logo }} />
           </div>
-          <div className="info-page-header-section">
-            <div>
-              <span>{programData.channelTitle}</span>
+          <div className="info-page-header-section info-page-header-tv-info">
+            <div className="info-tv-info-section">
+              <span className="info-page-header-white">
+                {programData.channelTitle}
+              </span>
               <span>{programData.parsedSchedule}</span>
               <span>{programData.parsedDate}</span>
             </div>
-            <div>{programData.title}</div>
-            <div>
+            <div className="info-page-header-white info-tv-info-section">
+              {programData.title}
+            </div>
+            <div className="info-tv-info-section">
               <span>{programData.meta.year}</span>
               {programData.meta.genres.length > 0 &&
-                programData.meta.genres.map((genre) => <span>{genre}</span>)}
+                programData.meta.genres.map((genre, index) => (
+                  <span key={`INFO_PAGE_GENRE_${genre}_${index}`}>{genre}</span>
+                ))}
             </div>
           </div>
         </div>
         <Expandable height={100}>{programData.description}</Expandable>
-        <div>
-          <div>
-            <span>Cast</span>
+        <div className="info-page-cast-container">
+          <div className="info-page-cast-section">
+            <span className="info-page-cast-title-section">
+              {literals.cast}
+            </span>
             {programData.meta.cast.length > 0 &&
-              programData.meta.cast.map((castItem) => (
-                <span>{castItem.name}</span>
+              programData.meta.cast.map((castItem, index) => (
+                <span key={`INFO_PAGE_CAST_${castItem.name}_${index}`}>
+                  {castItem.name}
+                </span>
               ))}
           </div>
-          <div>
-            <span>Creators</span>
+          <div className="info-page-cast-section">
+            <span className="info-page-cast-title-section">
+              {literals.creator}
+            </span>
             {programData.meta.creators.length > 0 &&
-              programData.meta.creators.map((creatorItem) => (
-                <span>{creatorItem.name}</span>
+              programData.meta.creators.map((creatorItem, index) => (
+                <span key={`INFO_PAGE_CREATOR_${creatorItem.name}_${index}`}>
+                  {creatorItem.name}
+                </span>
               ))}
           </div>
         </div>
